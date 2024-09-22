f276fe53bb557108aa31063ec3b587e3e3a2a7fd
Jaydatt Karan <karanjaydatt03@gmail.com>
2024-09-22 16:56:49 +0530

    week4 updated

--- WebDev/week4/index.js
+++ WebDev/week4/index.js
@@ -1,7 +1,26 @@
-import chalk from 'chalk';
+// import chalk from 'chalk';
 
-console.log(chalk.blue('Hello world!'));
-console.log(chalk.red.bold('This is an error message.'));
-console.log(chalk.green.underline('This is an success message.'));
+// console.log(chalk.blue('Hello world!'));
+// console.log(chalk.red.bold('This is an error message.'));
+// console.log(chalk.green.underline('This is an success message.'));
 
 // console.log(__dirname);
+
+
+const fs = require("fs");
+
+function main(fileName) {
+    fs.readFile(fileName, "utf-8", function(err, data) {
+        /// hello world
+        let total = 0;
+        for (let i=0; i<data.length; i++) {
+            if (data[i] === " "){
+                total++;
+            }
+        }
+        console.log(total);
+    })
+}
+
+// main("a.txt");
+main(process.argv[2]);
